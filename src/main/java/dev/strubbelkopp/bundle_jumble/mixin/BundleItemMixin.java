495550fc7431e73bac7243134c18cd79db5edd08
Strubbelkopp <nils130902@gmail.com>
2022-07-27 04:20:36 +0200

    Initial commit

---
+++ src/main/java/dev/strubbelkopp/bundle_jumble/mixin/BundleItemMixin.java
@@ -0,0 +1,144 @@
+package dev.strubbelkopp.bundle_jumble.mixin;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.*;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtElement;
+import net.minecraft.nbt.NbtList;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Mixin(BundleItem.class)
+public abstract class BundleItemMixin extends Item implements DyeableItem{
+
+    @Shadow @Final private static String ITEMS_KEY;
+    @Shadow @Final public static int MAX_STORAGE;
+    private static final int DEFAULT_COLOR = 0xcc7b46;
+    private static final String SEED_KEY = "Seed";
+
+    @Override
+    public int getColor(ItemStack stack) {
+        NbtCompound nbtCompound = stack.getSubNbt(DISPLAY_KEY);
+        if (nbtCompound != null && nbtCompound.contains(COLOR_KEY, NbtElement.NUMBER_TYPE)) {
+            return nbtCompound.getInt(COLOR_KEY);
+        }
+        return DEFAULT_COLOR;
+    }
+
+    @Shadow
+    private static int getBundleOccupancy(ItemStack stack) {
+        throw new IllegalCallerException();
+    }
+
+    @Shadow
+    private static int getItemOccupancy(ItemStack stack) {
+        throw new IllegalCallerException();
+    }
+
+    public BundleItemMixin(Settings settings) {
+        super(settings);
+    }
+
+    @Inject(method = "use", at = @At("HEAD"), cancellable = true)
+    public void use(World world, PlayerEntity user, Hand hand, CallbackInfoReturnable<TypedActionResult<ItemStack>> cir) {
+        if (!user.isSneaking()) {
+            cir.setReturnValue(TypedActionResult.fail(user.getStackInHand(hand)));
+        }
+    }
+
+    @Override
+    public ActionResult useOnBlock(ItemUsageContext context) {
+        PlayerEntity player = context.getPlayer();
+        if (player != null && !player.isSneaking()) {
+            ItemStack bundleItemStack = player.getStackInHand(context.getHand());
+            NbtCompound bundleNbtCompound = bundleItemStack.getOrCreateNbt();
+            if (bundleNbtCompound.contains(ITEMS_KEY)) {
+                World world = context.getWorld();
+                NbtList nbtList = bundleNbtCompound.getList(ITEMS_KEY, NbtElement.COMPOUND_TYPE);
+                List<Integer> availableIndexes = containedBlockIndexes(nbtList);
+                ActionResult result = ActionResult.FAIL;
+                while (!availableIndexes.isEmpty() && !(result == ActionResult.success(world.isClient))) {
+                    Random random = world.getRandom();
+                    random.setSeed(bundleNbtCompound.getLong(SEED_KEY));
+                    bundleNbtCompound.putLong(SEED_KEY, random.nextLong());
+                    int index = random.nextInt(availableIndexes.size());
+                    int compoundIndex = availableIndexes.get(index);
+                    NbtCompound nbtCompound = nbtList.getCompound(compoundIndex);
+                    ItemStack itemStack = ItemStack.fromNbt(nbtCompound);
+                    BlockItem blockItem = (BlockItem) itemStack.getItem();
+                    result = blockItem.useOnBlock(context);
+                    if (result == ActionResult.success(world.isClient)) {
+                        if (!player.isCreative()) {
+                            ItemStack copyItemStack = itemStack.copy();
+                            nbtList.remove(nbtCompound);
+                            itemStack.decrement(1);
+                            itemStack.writeNbt(nbtCompound);
+                            if (itemStack.getCount() == 0) {
+                                if (!tryRefillItemStack(player, copyItemStack, bundleItemStack, nbtList)) {
+                                    Text outOfItemMessage = Text.translatable("text.bundle_jumble.bundle.out_of_item", Text.translatable(blockItem.getTranslationKey()));
+                                    player.sendMessage(outOfItemMessage, true);
+                                }
+                            } else {
+                                nbtList.add(0, nbtCompound);
+                            }
+                            if (nbtList.isEmpty()) {
+                                bundleItemStack.removeSubNbt(ITEMS_KEY);
+                            }
+                        }
+                        return result;
+                    } else {
+                        availableIndexes.remove(index);
+                    }
+                }
+            }
+        }
+        return super.useOnBlock(context);
+    }
+
+    static private Boolean tryRefillItemStack(PlayerEntity player, ItemStack itemStack, ItemStack bundleItemStack, NbtList nbtList) {
+        PlayerInventory inventory = player.getInventory();
+        for (int i = 9; i < 36; i++) { //only search for blocks that are in the 3x9 space above the hotbar (index 9-36)
+            ItemStack inventoryItemStack = inventory.getStack(i);
+            if (ItemStack.canCombine(inventoryItemStack, itemStack)) {
+                int j = getBundleOccupancy(bundleItemStack);
+                int k = getItemOccupancy(inventoryItemStack);
+                int l = Math.min(inventoryItemStack.getCount(), (MAX_STORAGE - j) / k);
+                ItemStack newItemStack = inventoryItemStack.copy();
+                newItemStack.setCount(l);
+                NbtCompound newNbtCompound = new NbtCompound();
+                newItemStack.writeNbt(newNbtCompound);
+                nbtList.add(0, newNbtCompound);
+                inventory.removeStack(i, l);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private List<Integer> containedBlockIndexes(NbtList nbtList) {
+        List<Integer> availableIndexes = new ArrayList<>();
+        for (int i = 0; i < nbtList.size(); i++) {
+            NbtCompound nbtCompound = nbtList.getCompound(i);
+            ItemStack itemStack =  ItemStack.fromNbt(nbtCompound);
+            Item item = itemStack.getItem();
+            if (item instanceof BlockItem) {
+                availableIndexes.add(i);
+            }
+        }
+        return availableIndexes;
+    }
+}
