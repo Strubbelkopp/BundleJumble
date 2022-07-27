91a546c7bfe008a61fe9a226fd0fde33c5da18a1
Strubbelkopp <nils130902@gmail.com>
2022-07-27 22:35:55 +0200

    A little cleanup

--- src/main/java/dev/strubbelkopp/bundle_jumble/mixin/BlockItemMixin.java
+++ src/main/java/dev/strubbelkopp/bundle_jumble/mixin/BlockItemMixin.java
@@ -2,7 +2,6 @@ package dev.strubbelkopp.bundle_jumble.mixin;
 
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.BundleItem;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,8 +11,7 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 public class BlockItemMixin {
     @Redirect(method = "place(Lnet/minecraft/item/ItemPlacementContext;)Lnet/minecraft/util/ActionResult;", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;decrement(I)V"))
     private void cancelBundleDecrement(ItemStack itemStack, int amount) {
-        Item item = itemStack.getItem();
-        if (item instanceof BundleItem) {
+        if (itemStack.getItem() instanceof BundleItem) {
             itemStack.decrement(0);
         } else {
             itemStack.decrement(amount);
